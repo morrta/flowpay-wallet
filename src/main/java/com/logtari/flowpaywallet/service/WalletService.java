ba611a5a6349ef4edb8ba3496456c07ff6b9d56d
morrta <mohamedmortadha.logtari@gmail.com>
2026-01-14 22:34:39 +0100

    feat: naive implementation to wallet deposit functionality

--- src/main/java/com/logtari/flowpaywallet/service/WalletService.java
+++ src/main/java/com/logtari/flowpaywallet/service/WalletService.java
@@ -4,11 +4,7 @@ import com.logtari.flowpaywallet.common.WalletNotFoundException;
 import com.logtari.flowpaywallet.entity.Wallet;
 import com.logtari.flowpaywallet.repository.WalletRespository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.math.BigDecimal;
-import java.sql.Timestamp;
 import java.time.Instant;
 import java.util.UUID;
 
